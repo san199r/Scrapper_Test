13b0d8ffe894781fbeac95f3f56d68b80650731a
Bob <psantosh1999@gmail.com>
2026-04-19 13:13:05 +0530

    Remove webdriver-manager and switch to Selenium-native driver management to fix WinError 193

--- Lexington_KY_Permits_DateRange_Tool.py
+++ Lexington_KY_Permits_DateRange_Tool.py
@@ -12,7 +12,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 SEARCH_URL = "https://aca-prod.accela.com/LEXKY/Cap/CapHome.aspx?module=Building&TabName=Building"
@@ -87,8 +86,7 @@ def create_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(120)
     return driver
 
